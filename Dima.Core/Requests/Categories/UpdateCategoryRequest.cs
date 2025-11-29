58b2c60843828708f26085729b7f28ee786c8ef9
sjondev <jonsilvasantos7@gmail.com>
2025-11-29 18:55:47 -0300

    standardizing the category request

---
+++ Dima.Core/Requests/Categories/UpdateCategoryRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dima.Core.Requests.Categories;
+
+public class UpdateCategoryRequest : Request
+{
+    [Required(ErrorMessage = "Invalid Title")]
+    [MaxLength(80, ErrorMessage = "Maximum allowed length is 80")]
+    public string Title { get; set; }  = string.Empty;
+    
+    [Required(ErrorMessage = "Invalid description")]
+    [MaxLength(200, ErrorMessage = "Maximum allowed length is 200")]
+    public string Description { get; set; }  = string.Empty;
+}
