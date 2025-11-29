a63445b543b025e2bdf4ef2bf84b76d07d70f75a
sjondev <jonsilvasantos7@gmail.com>
2025-11-29 17:58:08 -0300

    add a folder request and response default code arquiteture

---
+++ Dima.Core/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace Dima.Core;
+
+public class Configuration
+{
+    public const int DEFAULT_STATUS_CODE = 200;
+    public const int DefaultPageNumber = 1;
+    public const int DefaultPageSize = 25;
+}
---
+++ Dima.Core/Responses/Response.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Dima.Core.Responses;
+
+public class Response<TData>
+{
+    private readonly int _code;
+
+    // parameterless
+    [JsonConstructor]
+    public Response() => _code = Configuration.DEFAULT_STATUS_CODE;
+
+    public Response(
+        TData? data, 
+        int code = Configuration.DEFAULT_STATUS_CODE, 
+        string? message = "")
+    {
+        Data = data;
+        Message = message;
+        _code = code;
+    }
+    
+    public TData Data { get; set; }
+    public string? Message { get; set; }
+    
+    [JsonIgnore]
+    public bool isSuccess => _code is >= 200 and <= 299;
+}
